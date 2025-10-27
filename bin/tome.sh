be50d06e1d6965c689375e8a9cd6b6d7da290a30
Blue Falcon <anon@hostpi.onmicrosoft.com>
2025-10-27 07:08:54 -0700

    switch to sh for compat

--- bin/tome.sh
+++ bin/tome.sh
@@ -1,21 +1,31 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 scriptroot=$(dirname "$(realpath "$0")")
-
-# Resolve path to the real tome executable (avoid recursion)
 real_tome="$scriptroot/tome.js"
+
+case " $* " in
+  *" --stdout "*) has_stdout=1 ;;
+  *) has_stdout=0 ;;
+esac
+
+args=
+for arg in "$@"; do
+  [ "$arg" = "--stdout" ] || args="$args \"$arg\""
+done
+
+eval "set -- $args"
 
 # Detect presence of --stdout among arguments and handle stdin accordingly
 if [ -t 0 ]; then
-    if [[ " $* " == *" --stdout "* ]]; then
-        exec "$real_tome" "$1" 2>/dev/tty
+    if [ "$has_stdout" -eq 1 ]; then
+        exec "$real_tome" "$1" --stdout 2>/dev/tty
     else
         exec "$real_tome" "$1"
     fi
 else
     input_data=$(cat)
-    if [[ " $* " == *" --stdout "* ]]; then
-        exec "$real_tome" "$1" "$input_data" 2>/dev/tty < /dev/tty
+    if [ "$has_stdout" -eq 1 ]; then
+        exec "$real_tome" "$1" "$input_data" --stdout 2>/dev/tty < /dev/tty
     else
         exec "$real_tome" "$1" "$input_data" < /dev/tty
     fi
